090ed7ed8a18ba3d95878a3093ac5266acf1d91d
Sakralbar0192 <sakralbar0192@gmail.com>
2021-02-01 19:48:38 +0800

    исправил именование функции

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let RANDOM_NUMBER = function getRandomNumber(min = 0, max = 1000, precision = 5) {
+const RANDOM_NUMBER = function (min = 0, max = 1000, precision = 5) {
   if (min < 0 || max < 0 || precision < 0) {
     min = Math.sqrt(Math.pow(min));
     max = Math.sqrt(Math.pow(max));
@@ -11,7 +11,5 @@ let RANDOM_NUMBER = function getRandomNumber(min = 0, max = 1000, precision = 5)
     return (((Math.random()* (max-min))+ min).toFixed(precision))
   }
 }
-
-//Линтер ругается на консоль и на функцию/переменную, которые обьявлены, но не используются - поэтому использую алерт
 
 alert(RANDOM_NUMBER())
